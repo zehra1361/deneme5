4b3f44a01bdea52df7d8a82731bdcec9274c36a2
zehra1361 <zehra1361>
2020-11-20 19:07:39 +0200

    zehradan mastera

--- src/deneme5/FirstClass.java
+++ src/deneme5/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 	System.out.println("birinci komut");	
-
+	System.out.println("ikinci komut ");
+	System.out.println("ucuncu komut");		
+	System.out.println("dorduncu komut");
 	}
 
 }
